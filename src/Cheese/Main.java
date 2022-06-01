625d38b50c94fd10273b3659ae1c4709ee55e121
Sara Gonzalez <sxga2003@gmail.com>
2022-06-01 11:53:34 +0200

    COMMIT master MAIN

--- src/Cheese/Main.java
+++ src/Cheese/Main.java
@@ -8,12 +8,22 @@ package Cheese;
 
 /**
  *
- * @author ___
+ * @author Sara
  */
 public class Main {
     
      public static void main(String[] args) {
     
-         System.out.println("1º Gdam: Entornos de desarrollo");
+        System.out.println("1º Gdam: Entornos de desarrollo");
+         
+        String cod1 = "001";        
+        Producto prod1 = new Producto(cod1, "semidesnatada", 2, 1.2);
+         
+        String cod2 = "002";        
+        Producto prod2 = new Producto(cod2, "entera", 3, 1.2);
+         
+        System.out.println("Listado de productos:");
+        System.out.println();
+        }
+        
     }
-}
